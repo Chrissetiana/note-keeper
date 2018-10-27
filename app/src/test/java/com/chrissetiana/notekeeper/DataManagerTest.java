e4695a35aadd91ad5dc98574108df519a0084939
chrissetiana <chrissetiana@gmail.com>
2018-10-27 13:19:35 +0100

    Test one-step-create notes

--- app/src/test/java/com/chrissetiana/notekeeper/DataManagerTest.java
+++ app/src/test/java/com/chrissetiana/notekeeper/DataManagerTest.java
@@ -1,34 +1,39 @@
 package com.chrissetiana.notekeeper;
 
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class DataManagerTest {
+    static DataManager dataManager;
+
+    @BeforeClass
+    public static void classSetup() {
+        dataManager = DataManager.getInstance();
+    }
 
     @Before
     public void setup() {
-        DataManager dm = DataManager.getInstance();
-        dm.getNotes().clear();
-        dm.initializeExampleNotes();
+        dataManager.getNotes().clear();
+        dataManager.initializeExampleNotes();
     }
 
     @Test
     public void createNewNote() {
-        final DataManager dm = DataManager.getInstance();
-        final CourseInfo course = dm.getCourse("android_sync");
+        final CourseInfo course = dataManager.getCourse("android_sync");
         final String title = "Test Note Title";
         final String text = "This is the body of my test note";
 
-        int noteIndex = dm.createNewNote();
+        int noteIndex = dataManager.createNewNote();
 
-        NoteInfo newNote = dm.getNotes().get(noteIndex);
+        NoteInfo newNote = dataManager.getNotes().get(noteIndex);
         newNote.setCourse(course);
         newNote.setTitle(title);
         newNote.setText(text);
 
-        NoteInfo compareNotes = dm.getNotes().get(noteIndex);
+        NoteInfo compareNotes = dataManager.getNotes().get(noteIndex);
         assertEquals(course, compareNotes.getCourse());
         assertEquals(title, compareNotes.getTitle());
         assertEquals(text, compareNotes.getText());
@@ -36,28 +41,40 @@ public class DataManagerTest {
 
     @Test
     public void findSimilarNotes() {
-        final DataManager dm = DataManager.getInstance();
-        final CourseInfo course = dm.getCourse("android_sync");
+        final CourseInfo course = dataManager.getCourse("android_sync");
         final String title = "Test Note Title";
         final String text1 = "This is the body of my test note";
         final String text2 = "This is the body of my second test note";
 
-        int noteIndex1 = dm.createNewNote();
-        NoteInfo newNote1 = dm.getNotes().get(noteIndex1);
+        int noteIndex1 = dataManager.createNewNote();
+        NoteInfo newNote1 = dataManager.getNotes().get(noteIndex1);
         newNote1.setCourse(course);
         newNote1.setTitle(title);
         newNote1.setText(text1);
 
-        int noteIndex2 = dm.createNewNote();
-        NoteInfo newNote2 = dm.getNotes().get(noteIndex2);
+        int noteIndex2 = dataManager.createNewNote();
+        NoteInfo newNote2 = dataManager.getNotes().get(noteIndex2);
         newNote2.setCourse(course);
         newNote2.setTitle(title);
         newNote2.setText(text2);
 
-        int foundIndex1 = dm.findNote(newNote1);
+        int foundIndex1 = dataManager.findNote(newNote1);
         assertEquals(noteIndex1, foundIndex1);
 
-        int foundIndex2 = dm.findNote(newNote1);
+        int foundIndex2 = dataManager.findNote(newNote1);
         assertEquals(noteIndex2, foundIndex2);
+    }
+
+    @Test
+    public void createNewNoteOneStepCreation() {
+        final CourseInfo course = dataManager.getCourse("android_async");
+        final String title = "Test Note Title";
+        final String text = "This is the body of my test note";
+
+        int index = dataManager.createNewNote(course, title, text);
+        NoteInfo compareNote = dataManager.getNotes().get(index);
+        assertEquals(course, compareNote.getCourse());
+        assertEquals(title, compareNote.getTitle());
+        assertEquals(text, compareNote.getText());
     }
 }
