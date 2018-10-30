00075e8c030b2fb2c1199c52688d1cfdcce1d0cb
chrissetiana <chrissetiana@gmail.com>
2018-10-30 23:29:00 +0100

    Add test [data interaction]

--- app/src/androidTest/java/com/chrissetiana/notekeeper/NoteCreationTest.java
+++ app/src/androidTest/java/com/chrissetiana/notekeeper/NoteCreationTest.java
@@ -8,22 +8,34 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.closeSoftKeyboard;
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.instanceOf;
 
 @RunWith(AndroidJUnit4.class)
 public class NoteCreationTest {
+
+    static DataManager dataManager;
 
     @Rule
     public ActivityTestRule<NoteListActivity> rule = new ActivityTestRule<>(NoteListActivity.class);
 
     @Test
     public void createNewNote() {
+        final CourseInfo course = dataManager.getCourse("java_lang");
+        final String noteTitle = "Test note title";
+        final String noteText = "This is the body of our test note";
+
         onView(withId(R.id.fab)).perform(click());
-        onView(withId(R.id.text_title)).perform(typeText("Test note title"));
-        onView(withId(R.id.text_note)).perform(typeText("This is the body of our test note"));
-        closeSoftKeyboard();
+        onView(withId(R.id.spinner_topic)).perform(click());
+
+        onData(allOf(instanceOf(CourseInfo.class), equalTo(course))).perform(click());
+        onView(withId(R.id.text_title)).perform(typeText(noteTitle));
+        onView(withId(R.id.text_note)).perform(typeText(noteText), closeSoftKeyboard();
     }
 }
