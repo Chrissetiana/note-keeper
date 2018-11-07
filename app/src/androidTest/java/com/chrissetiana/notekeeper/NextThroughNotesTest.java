dfc036bace883cd820604dd6329ebdbae1f9beb6
chrissetiana <chrissetiana@gmail.com>
2018-11-07 15:44:20 +0100

    Test: navigation drawer

---
+++ app/src/androidTest/java/com/chrissetiana/notekeeper/NextThroughNotesTest.java
@@ -0,0 +1,17 @@
+package com.chrissetiana.notekeeper;
+
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+public class NextThroughNotesTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void NextThroughNotes() {
+
+    }
+}
