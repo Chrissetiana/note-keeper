3bedae9b064646952fe0e96d9a14a5ec50e46a21
chrissetiana <chrissetiana@gmail.com>
2018-11-15 15:41:46 +0100

    Edit: set preference defaults

--- app/src/main/java/com/chrissetiana/notekeeper/MainActivity.java
+++ app/src/main/java/com/chrissetiana/notekeeper/MainActivity.java
@@ -2,6 +2,7 @@ package com.chrissetiana.notekeeper;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
@@ -42,6 +43,10 @@ public class MainActivity extends AppCompatActivity
                 startActivity(new Intent(MainActivity.this, NoteActivity.class));
             }
         });
+
+        PreferenceManager.setDefaultValues(this, R.xml.pref_general, false);
+        PreferenceManager.setDefaultValues(this, R.xml.pref_notification, false);
+        PreferenceManager.setDefaultValues(this, R.xml.pref_data_sync, false);
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
