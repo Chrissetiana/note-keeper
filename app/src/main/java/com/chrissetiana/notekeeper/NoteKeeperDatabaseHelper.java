ab98caa08c7403577b5850ea3587a4dd863b1000
Tiana Aiyemo <chrissetiana@gmail.com>
2019-01-17 14:27:00 +0100

    Add: indexes in db

--- app/src/main/java/com/chrissetiana/notekeeper/NoteKeeperDatabaseHelper.java
+++ app/src/main/java/com/chrissetiana/notekeeper/NoteKeeperDatabaseHelper.java
@@ -4,10 +4,13 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import com.chrissetiana.notekeeper.NoteKeeperDatabaseContract.CourseInfoEntry;
+import com.chrissetiana.notekeeper.NoteKeeperDatabaseContract.NoteInfoEntry;
+
 class NoteKeeperDatabaseHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "notekeeper.db";
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 2;
 
     NoteKeeperDatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -15,8 +18,11 @@ class NoteKeeperDatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(NoteKeeperDatabaseContract.CourseInfoEntry.SQL_CREATE_TABLE);
-        db.execSQL(NoteKeeperDatabaseContract.NoteInfoEntry.SQL_CREATE_TABLE);
+        db.execSQL(CourseInfoEntry.SQL_CREATE_TABLE);
+        db.execSQL(NoteInfoEntry.SQL_CREATE_TABLE);
+
+        db.execSQL(CourseInfoEntry.SQL_CREATE_INDEX1);
+        db.execSQL(NoteInfoEntry.SQL_CREATE_INDEX1);
 
         DatabaseDataWorker dataWorker = new DatabaseDataWorker(db);
         dataWorker.insertCourses();
@@ -25,6 +31,9 @@ class NoteKeeperDatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        if (oldVersion < 2) {
+            db.execSQL(CourseInfoEntry.SQL_CREATE_INDEX1);
+            db.execSQL(NoteInfoEntry.SQL_CREATE_INDEX1);
+        }
     }
 }
