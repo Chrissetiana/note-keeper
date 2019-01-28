6df93cce806706bb7c4967bf6e6474c9c0a3a0c9
Tiana Aiyemo <chrissetiana@gmail.com>
2019-01-28 13:48:24 +0100

    Edit: provider contract

--- app/src/main/java/com/chrissetiana/notekeeper/NoteKeeperProviderContract.java
+++ app/src/main/java/com/chrissetiana/notekeeper/NoteKeeperProviderContract.java
@@ -1,4 +1,23 @@
 package com.chrissetiana.notekeeper;
 
+import android.net.Uri;
+
 public final class NoteKeeperProviderContract {
+
+    public static final String AUTHORITY = "com.chrissetiana.notekeeper.provider";
+    public static final Uri AUTHORITY_URI = Uri.parse("content://" + AUTHORITY);
+
+    public NoteKeeperProviderContract() {
+
+    }
+
+    public static final class Courses {
+        public static final String PATH = "courses";
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(AUTHORITY_URI, PATH);
+    }
+
+    public static final class Notes {
+        public static final String PATH = "notes";
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(AUTHORITY_URI, PATH);
+    }
 }
