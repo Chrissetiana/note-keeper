d4ccfc23b6ee8b8e960ec884f934dd5d0410abf8
Tiana Aiyemo <chrissetiana@gmail.com>
2019-01-28 13:58:01 +0100

    Add: contract interfaces

--- app/src/main/java/com/chrissetiana/notekeeper/NoteKeeperProviderContract.java
+++ app/src/main/java/com/chrissetiana/notekeeper/NoteKeeperProviderContract.java
@@ -1,6 +1,7 @@
 package com.chrissetiana.notekeeper;
 
 import android.net.Uri;
+import android.provider.BaseColumns;
 
 public final class NoteKeeperProviderContract {
 
@@ -11,12 +12,25 @@ public final class NoteKeeperProviderContract {
 
     }
 
-    public static final class Courses {
+    protected interface CourseIdColumns {
+        String COLUMN_COURSE_ID = "course_id";
+    }
+
+    protected interface CourseColumns {
+        String COLUMN_COURSE_TITLE = "course_title";
+    }
+
+    protected interface NotesColumns {
+        String COLUMN_NOTE_TITLE = "note_title";
+        String COLUMN_NOTE_TEXT = "note_text";
+    }
+
+    public static final class Courses implements BaseColumns, CourseIdColumns, CourseColumns {
         public static final String PATH = "courses";
         public static final Uri CONTENT_URI = Uri.withAppendedPath(AUTHORITY_URI, PATH);
     }
 
-    public static final class Notes {
+    public static final class Notes implements BaseColumns, CourseIdColumns, NotesColumns {
         public static final String PATH = "notes";
         public static final Uri CONTENT_URI = Uri.withAppendedPath(AUTHORITY_URI, PATH);
     }
